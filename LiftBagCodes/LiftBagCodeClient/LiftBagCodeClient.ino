8ef1cdc36a2d132e8ba01c64457bddb9b60e868e
Ankit Jha <37044020+AnkitJha1998@users.noreply.github.com>
2018-04-20 01:11:25 +0530

    Changed OBS server and LiftBag codes

--- LiftBagCodes/LiftBagCodeClient/LiftBagCodeClient.ino
+++ LiftBagCodes/LiftBagCodeClient/LiftBagCodeClient.ino
@@ -4,6 +4,10 @@ const char* ssid="LiftBag";
 const char* host="192.168.0.4";
 WiFiClient client;
 bool connectStatus=false;
+
+//int dataLiftBag=0;
+//int connectBit=2;
+
 void connecthotspot()
 {
   WiFi.begin(ssid);
@@ -16,12 +20,37 @@ void connecthotspot()
   connectStatus=true;
 }
 
+int switchh()
+{
+  char c=Serial.read();
+  String s;
+  s+=c;
+  int i=s.toInt();
+  return i;
+}
+
+/*int switchCase(int rw)
+{
+  switch(rw)
+  {
+    case 0: return 5;
+    case 1: return 6;
+    case 2: return 7;
+    case 3: return 1;
+    default: return 9; 
+  }
+}*/
+
+
+
 void endConnection()
 {
   client.stop();
   WiFi.disconnect();
-  delay(3000);
+  Serial.println("Reconnecting after 6 seconds");
+  delay(6000);
   connectStatus=false;
+  //connectHotspot2();
   connecthotspot();                         //again initialise the hotsopt
 }
 
@@ -32,6 +61,7 @@ void setup() {
 void loop() {
   
   int data=0;                                                     //replace d with switch case
+  
   if(data==0&&connectStatus==false)connecthotspot();
   String ss="";
   if(client.connect(host,80))
@@ -39,6 +69,7 @@ void loop() {
     Serial.println("Connected");
     while(client.connected())
     {
+      data=switchh();
       Serial.println("Sending "+String(data));
       String d=String(data);
       client.println(d);                                        //sends the integer
@@ -46,11 +77,16 @@ void loop() {
       while(client.available())
       {
         String s3=client.readStringUntil('\r');                 //Reads the response
-        ss+=s3;
-        if(s3[0]!='\0')break;
+        ss=s3;
+        
       }
-      Serial.println(ss);                                       //Just prints data for now
-       if(data==1)break;                                        
+      int sw=ss.toInt();
+      
+      
+      Serial.print("Received:");
+      Serial.println(ss+"\n");                                       //Just prints data for now
+       if(data==1){break;}
+      delay(300);
     }
     Serial.println("COnnection disabled");
     endConnection();
@@ -60,4 +96,5 @@ void loop() {
     Serial.println("Connection failed");
     client.stop();
   }
+  
 }
