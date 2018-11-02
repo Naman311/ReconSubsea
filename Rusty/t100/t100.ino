82a774b918aa1dbb943b6a64f10d085656e7011b
SauravCR7 <sauravsaha48@gmail.com>
2018-11-02 17:40:50 +0530

    mod

--- Rusty/t100/t100.ino
+++ Rusty/t100/t100.ino
@@ -6,17 +6,17 @@
 //fl=5
 //rr=6
 //fr=7
-//byte servoPin = 3;9
-//Servo servo;
-//byte servoPin1 = 5;10
-//Servo servo1;
-//byte servoPin2 = 6;11
-//Servo servo2;
-byte servoPin3 = 3;
+byte servoPin = 3;    //LU
+Servo servo;
+byte servoPin1 = 5;
+Servo servo1;
+byte servoPin2 = 6;     //LB
+Servo servo2;
+byte servoPin3 = 4;
 Servo servo3;
-byte servoPin4 = 5;
+byte servoPin4 = 7;
 Servo servo4;
-byte servoPin5 =6;
+byte servoPin5 = 8;
 Servo servo5;
 
 /*byte servoPin2 = 12;
@@ -32,10 +32,10 @@ byte servoPin4 = 5;
 Servo servo4;
 */
 void setup() {
-  //servo1.attach(servoPin1);
-  //servo.attach(servoPin);
-  //servo1.attach(servoPin1);
-  //servo2.attach(servoPin2);
+  servo1.attach(servoPin1);
+  servo.attach(servoPin);
+  servo1.attach(servoPin1);
+  servo2.attach(servoPin2);
   servo3.attach(servoPin3);
   servo4.attach(servoPin4);
   servo5.attach(servoPin5);
@@ -43,9 +43,9 @@ void setup() {
   servo3.attach(servoPin3);
   servo4.attach(servoPin4);
   */
-  //servo.writeMicroseconds(1500);
-  //servo1.writeMicroseconds(1500);
-  //servo2.writeMicroseconds(1500);
+  servo.writeMicroseconds(1500);
+  servo1.writeMicroseconds(1500);
+  servo2.writeMicroseconds(1500);
   servo3.writeMicroseconds(1500);
   servo4.writeMicroseconds(1500);
   servo5.writeMicroseconds(1500);// send "stop" signal to ESC.
@@ -58,10 +58,10 @@ void setup() {
 
 void loop() 
 {
-  int signal= 1600;
-  //servo.writeMicroseconds(signal);
-  //servo1.writeMicroseconds(signal);
-  //servo2.writeMicroseconds(signal);
+  int signal= 1520;
+  servo.writeMicroseconds(signal);
+  servo1.writeMicroseconds(signal);
+  servo2.writeMicroseconds(signal);
   servo3.writeMicroseconds(signal);
   servo4.writeMicroseconds(signal);
   servo5.writeMicroseconds(signal);// Send signal to ESC.
@@ -71,9 +71,9 @@ void loop()
   servo4.writeMicroseconds(signal); // Send signal to ESC.*/
   delay(2000);
   signal=1500;
-  //servo.writeMicroseconds(signal);
-  //servo1.writeMicroseconds(signal);
-  //servo2.writeMicroseconds(signal);
+  servo.writeMicroseconds(signal);
+  servo1.writeMicroseconds(signal);
+  servo2.writeMicroseconds(signal);
   servo3.writeMicroseconds(signal);
   servo4.writeMicroseconds(signal);
   servo5.writeMicroseconds(signal);// Send signal to ESC.
@@ -81,5 +81,15 @@ void loop()
   servo2.writeMicroseconds(signal); // Send signal to ESC.
   servo3.writeMicroseconds(signal); // Send signal to ESC.
   servo4.writeMicroseconds(signal); // Send signal to ESC.*/
+  signal=1520;
+  delay(2000);
+  servo.writeMicroseconds(signal);
+  servo1.writeMicroseconds(signal);
+  servo2.writeMicroseconds(signal);
+  delay(2000);
+  signal=1500;
+  servo.writeMicroseconds(signal);
+  servo1.writeMicroseconds(signal);
+  servo2.writeMicroseconds(signal);
   delay(2000);
 }
